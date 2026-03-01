f3f86eadb0e419547eecf04c496395a0ef1b178c
HaGa IT Solution <nengkya@yahoo.com>
2026-03-02 03:47:45 +0700

    vercel deploy has no free full template

---
+++ greeter.js
@@ -0,0 +1,3 @@
+function greeter(person){return"Typescript 5.9 with "+person}
+var user="HaGa IT Solution"
+document.body.textContent=greeter(user)
---
+++ greeter.ts
@@ -0,0 +1,3 @@
+function greeter(person){return"Typescript 5.9 with "+person}
+const user="HaGa IT Solution"
+document.body.textContent=greeter(user)
