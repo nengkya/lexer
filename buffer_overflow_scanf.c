c66796f512a5d1621c0823424693be059a041ea5
HaGa IT Solution <nengkya@yahoo.com>
2026-05-23 07:33:18 +0700

    django-admin startproject mysite djangotutorial

---
+++ buffer_overflow_scanf.c
@@ -0,0 +1,12 @@
+/*scanf*/
+#include"stdio.h"
+
+
+int main(){
+
+    /*1234/0*/
+    char name [5];
+
+    scanf("%s", name);
+
+}
---
+++ half_triangle.c
@@ -0,0 +1,18 @@
+#include"stdio.h"
+
+
+int main(){
+
+    int rows;
+
+
+    printf("Enter the number of triangle rows to be printed : ");
+
+    scanf("%d", &rows);
+
+    printf("%d\n", rows);
+
+
+
+
+}
